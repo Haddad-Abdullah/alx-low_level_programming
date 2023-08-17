f52759169c278489e2969062067718a1c411bfca
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-17 06:39:45 +0300

    more alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *printing alphabet 10 times
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	char print_alphabetx10(void)
+	{
+		int r;
+		for (r = 0; r < 11; r++)
+		{
+			for (i = 'a'; i <= 'z'; i++)
+			{
+				_putchar(i);
+			}
+		_putchar('/n');
+	}
+	return (0);
+}
+
