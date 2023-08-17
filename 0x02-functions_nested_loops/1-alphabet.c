4303c61de3d5ccdc57ae6453c43834a427b9fe81
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-17 06:18:17 +0300

    trying _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * printing the alphabet
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char print_alphabet(void)
+    {
+		int i;
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchat(i);
+		}
+		_putchar('\n');
+    }
+    return (0);
+}
+
