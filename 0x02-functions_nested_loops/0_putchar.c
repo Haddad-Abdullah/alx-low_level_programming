76c25650c6c2698d47f9157703b4a9da4bc23caf
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-17 05:18:22 +0300

    trying _putchar

--- 0x02-functions_nested_loops/0_putchar.c
+++ 0x02-functions_nested_loops/0_putchar.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
+#include "main.h"
+
 
 void main()
 {
-	char prt[] = '_putchar'
+	char prt[] = "_putchar"
+
 	for (i = 0; i < 8; i++){
 		_putchar(prt[i]);
 	}
 	_putchar('\n');
-
-	return(0);
+	return (0)
 }
+
