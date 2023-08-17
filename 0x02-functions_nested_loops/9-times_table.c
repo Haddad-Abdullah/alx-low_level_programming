d1a345fb6d52b50b36d7abedbe693399403cd106
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-17 23:25:02 +0300

    all done (I Hope) P.S. its not

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,12 +13,15 @@ void times_table(void)
 	int r;
 	int c;
 	int v = (r * c);
+
 	for (r = 0; r <= 10; r++)
 	{
 		for (c = 0; c <= 10; c++)
 		{
-			_putchar(v + ",\n");
+			_putchar(v);
+			_putchar(',');
+			_putchar(' ');
 		}
+		_putchar('\n');
 	}
-	return (0);
 }
