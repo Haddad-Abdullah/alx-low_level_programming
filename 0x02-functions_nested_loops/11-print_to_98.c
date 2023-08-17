c2de8e4e05a75cbb5e5599ad05677c7c803b8ce1
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-17 23:48:44 +0300

    all done (I Hope) P.S. its not

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,12 +13,13 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n; n <= 98; n++)
+		while (n < 98)
 		{
 			printf("%d", n);
+			n++;
 			if (n != 98)
 			{
-				printf(", ")
+				printf(", ");
 			}
 		}
 	}
@@ -27,12 +28,13 @@ void print_to_98(int n)
 		printf("%d", n);
 	}
 	else
-		for (n; n >= 98; n--)
+		while (n > 98)
 		{
 			printf("%d", n);
+			n--;
 			if (n != 98)
 			{
-				printf(", ")
+				printf(", ");
 			}
 		}
 	printf("\n");
