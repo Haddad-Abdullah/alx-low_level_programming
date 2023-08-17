dea3bcbeefa6cc1f015791546258d7225aea4725
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-17 19:42:00 +0300

    d

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * printing_alphabet_x10 
- * Description: 'printin 9 more alphaas'
- * Return: Always 0
+ * print_alphabet_x10 - Entry point
+ * Description: '0x02. C - Functions, nested loops'
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
