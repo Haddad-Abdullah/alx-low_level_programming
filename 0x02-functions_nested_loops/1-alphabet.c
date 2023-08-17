b1a86d56092bb3b40f850c3fd16fa73e017e1557
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-17 19:03:21 +0300

    o

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,17 +5,14 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    char print_alphabet(void)
-    {
-		int i;
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchat(i);
-		}
-		_putchar('\n');
-    }
-    return (0);
-}
 
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
