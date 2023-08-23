d4a84071c33d8293ea8301c742a36e96444db31b
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-23 05:54:19 +0300

    .

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+void print_rev(char *s)
+{
+	int n;
+	int m;
+	int end;
+
+	n = 0;
+
+	while (s[n] != '/0')
+	{
+		n++;
+	}
+
+	end = n;
+
+	for (m = end - 1; m >= 0; m--)
+	{
+		_putchar (s[m]);
+	}
+	_putchar ('\n');
+}
