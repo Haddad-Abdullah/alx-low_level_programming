841c88be3f8707e00958c18a25d905f116b1f54c
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-25 05:34:19 +0300

    commit 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strcat - Write a function that concatenates two strings.
+ *
+ *@dest: This is the output dest
+ *@src: This is the input source
+ *
+ * Return: This return to dest, that concatenates two strings
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	int c, c2;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+
+	for (c2 = 0; src[c2]; c2++)
+		dest[c++] = src[c2];
+
+	return (dest);
+}
