4d09db44b124dc29708ee351afafd128aa23bd3e
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-25 05:44:11 +0300

    --

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * reverse_array - main function
+ * @a: int
+ * @n: int
+ * Return: always 0
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
