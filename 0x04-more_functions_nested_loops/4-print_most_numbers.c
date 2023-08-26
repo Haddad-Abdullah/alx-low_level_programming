e4f54705456c5966128e481037f58c86eb02dff6
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-26 20:54:43 +0300

    5th

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,12 @@ void print_most_numbers(void)
 {
 	char i;
 
-	if (i != 50 && i != 52)
+	for (i = 48; i < 58; i++)
 	{
-		for (i = 48; i < 58;)
+		if (i != 50 && i != 52)
 		{
 			_putchar(i);
 			_putchar('\n');
 		}
-		i++
 	}
 }
