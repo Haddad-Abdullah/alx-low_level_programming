a40750b4f3d0ccf0ae080ad95e0893ac2a4c3862
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-27 01:39:33 +0300

    --

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a shape of 90 degree triangle
+ * @size: Size of the vertical triangle side
+ */
+
+void print_triangle(int size)
+{
+
+	for (rows = 1; rows <= size; rows++)
+	{
+		for (spc = size; spc > 0; spc--)
+			_putchar(' ');
+		for (i = 0; i < rows; i++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
