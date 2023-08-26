6cab126a3161b6eaa3b749803605347af4acc011
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-26 18:36:44 +0300

    2nd

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,6 +3,7 @@
 /**
  * _isdigit - checks if the char is a digit or not.
  * &c: the input as ascii.
+ * Return: always c
  */
 
 int _isdigit(int c)
@@ -10,6 +11,8 @@ int _isdigit(int c)
 	c = 0;
 
 	if (c > 47 && c < 58)
+	{
 		c = 1;
-
+	}
+	return (c);
 }
