da6d1fcae62bc0c0beed6160da8f90984d8b7aca
Abdullah_Usama <abdallah0usama@gmail.com>
2023-08-30 02:50:12 +0300

    1st

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,6 +6,7 @@
  * @s: destenation
  * @b: src
  * @n: number of bytes to be replaced
+ * Return: pointer to the dest. @s
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -13,6 +14,8 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int = i;
 
 	for (i = 0; i < n; i++)
+	{
 		s[i] = b;
+	}
 	return (s);
 }
